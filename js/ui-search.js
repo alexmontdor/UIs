cf0a667a8ab83f27facf2c5e68eb44cdca7a0e7f
Alexandre LANDREAU <alexandre@landreau.biz>
2018-01-07 14:03:56 +0100

    Add class to move the search ui

---
+++ js/ui-search.js
@@ -0,0 +1,10 @@
+var magnifier = document.getElementById('magnifier');
+magnifier.onclick = toggleAppearance;
+
+function toggleAppearance () {
+    var uiSearch = document.getElementById('ui-search');
+    if (uiSearch.classList.contains('open'))
+        uiSearch.classList.remove('open');
+    else
+        uiSearch.classList.add('open');
+}
