9bd95f4701ec566b0a23735497fbf6dff2c45dbd
Alexandre LANDREAU <alexandre@landreau.biz>
2018-01-07 23:22:08 +0100

    fix bug on JS

--- js/ui-selection-process.js
+++ js/ui-selection-process.js
@@ -9,11 +9,18 @@
  var input2= document.getElementById('input-step-2')
  var input3= document.getElementById('input-step-3')
 
+ var cancel1= document.getElementById('cancel-1')
+ var cancel2= document.getElementById('cancel-2')
+ var cancel3= document.getElementById('cancel-3')
 
- input1.onkeypress= checkEnter;
- input2.onkeypress= checkEnter;
- input3.onkeypress= checkEnter;
- 
+
+input1.onkeypress= checkEnter;
+input2.onkeypress= checkEnter;
+input3.onkeypress= checkEnter;
+
+cancel1.onclick = reset;
+cancel2.onclick = reset;
+cancel3.onclick = reset;
 
     function saveStep (id) {
 
@@ -41,3 +48,35 @@
         if (charCode == 13)
             saveStep(stepInProcess);
     };
+
+    function reset() {
+        var el = this
+        var id = el.id.slice(el.id.length-1, el.id.length)
+        console.log (el.id, id)
+        stepInProcess= parseInt(id)
+        var arrow, el
+        for (var i = stepInProcess; i <4; i++) {
+            arrow = document.getElementById('arrow-'+i.toString())
+            el = document.getElementById('input-step-'+i.toString())
+            el.style.display = 'none';
+            arrow.style.display = 'none';
+
+        }
+        
+            initStep (stepInProcess) 
+
+    }
+    function initStep (i) {
+        var el, arrow;
+        
+        el = document.getElementById('input-step-'+i.toString())
+        if (stepInProcess == 1) {
+            el.style.display = '';
+        }
+        if (stepInProcess >1) {
+            arrow = document.getElementById('arrow-'+(i-1).toString())
+            el.style.display = '';
+            arrow.style.display = '';
+        }
+
+    }
