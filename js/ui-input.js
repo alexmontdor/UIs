ea804c3673461c9355ad806b7393c6735e9cf67c
Alexandre LANDREAU <alexandre@landreau.biz>
2018-01-07 12:41:36 +0100

    complete JS code Add bold to entry Clean code

--- js/ui-input.js
+++ js/ui-input.js
@@ -1,1 +1,24 @@
-// if class is has something then set focus
+/**
+ * Focus all document that has a value
+ */
+var inputs = document.getElementsByClassName("ui-input");
+for ( let i=0; i < inputs.length; i++) {
+   changeInput(inputs[i])
+    inputs[i].onblur = changeInput
+}
+
+function changeInput(item) {
+    var elem
+    if (item.target)
+        elem = this;
+    else
+        elem = item
+    // console.log ('treating el', item, elem);
+     if (elem.value){
+        elem.classList.add("has-value");
+    }
+    else {
+        elem.classList.remove("has-value");
+    } 
+}
+
